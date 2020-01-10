896e1dba9cc56e429349c6bc6c716c431299aaee
Luis G. Matehuala Juarez <lg.mj9520@gmail.com>
2020-01-10 00:50:48 -0600

    obtencion de datos para el about en el servicio de firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -11,16 +11,30 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  url = 'https://portafolio-26c79.firebaseio.com/equipo.json';
+
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
     console.log('Servicio info pagina cargado');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     // Leer archivo Json
-    http.get('assets/data/data-pagina.json')
+    this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
         console.log(resp);
       } );
+  }
 
+  private cargarEquipo() {
+    this.http.get(`${this.url}`).subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
   }
 }
